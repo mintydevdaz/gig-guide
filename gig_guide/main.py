f009a52da8cf50d1efc873c9e7b4783b71feca07
mintydevdaz <mintydevdaz@proton.me>
2023-01-23 23:09:30 +1100

    C#28: created file

---
+++ gig_guide/main.py
@@ -0,0 +1,35 @@
+from datetime import datetime
+
+import pandas as pd
+from century import century
+from moshtix import moshtix
+from phoenix import phoenix
+from soh import sydney_opera_house
+
+
+def main():
+    df1 = moshtix()
+    df2 = phoenix()
+    df3 = sydney_opera_house()
+    df4 = century()
+
+    # Combine DataFrames
+    res = pd.concat([df1, df2])
+
+    # Sort by datetime column then remove it
+    res.sort_values(by="DT", inplace=True)
+    final_table = res.drop("DT", axis="columns")
+
+    write_csv(final_table)
+
+
+def write_csv(df):
+    now = datetime.now()
+    dt_string = now.strftime("%Y-%m-%d_%H%M%S")
+    filepath = f"/Users/darrenchung/Desktop/gigs_{dt_string}.csv"
+    print(f"CSV file saved at {filepath}")
+    return df.to_csv(filepath, index=False)
+
+
+if __name__ == "__main__":
+    main()
