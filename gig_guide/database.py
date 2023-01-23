82c3cfb902d3386c5f6159047da833d012f7a0e3
mintydevdaz <mintydevdaz@proton.me>
2023-01-23 14:38:25 +1100

    C#19: added phoenix url

--- gig_guide/database.py
+++ gig_guide/database.py
@@ -6,6 +6,8 @@ moshtix_links = {
     "Lansdowne Hotel": "https://www.moshtix.com.au/v2/venues/the-lansdowne-hotel-sydney/4775",
 }
 
+phoenix_link = {"Phoenix Central Park": "https://phoenixcentralpark.com.au/season-vii"}
+
 century_links = {
     "Enmore Theatre": "https://www.enmoretheatre.com.au/?s&key=upcoming",
     "Metro Theatre": "https://www.metrotheatre.com.au/?s&key=upcoming",
