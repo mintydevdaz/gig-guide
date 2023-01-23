9ceda6f62cfc2c96389b3f93169cf74815176ab7
mintydevdaz <mintydevdaz@proton.me>
2023-01-23 14:39:35 +1100

    C#21: added data dict

--- gig_guide/phoenix.py
+++ gig_guide/phoenix.py
@@ -1,86 +1,85 @@
 import itertools
 import re
+import sys
 from datetime import datetime
 
 import bs4
+import pandas as pd
 import requests
 from bs4 import BeautifulSoup
+from database import phoenix_link
 
 
 def main():
-    # Request HTML
-    main_page = "https://phoenixcentralpark.com.au/season-vii"
-    response = get_response(url=main_page)
-
-    # Parse HTML
-    soup = parse(r=response)
-
-    # Extract individual event URLs
+    # Get HTML response & parse. Extract invidiual event URLs.
+    link = phoenix_link.get("Phoenix Central Park")
+    r = get_html_response(url=link)
+    soup = parse_html(response=r)
     urls = get_urls(soup)
 
-    # Extract event details
+    # Cycle through each url
     dates = []
     events = []
     for url in urls:
-        # Get HTML & parse
-        response = get_response(url)
-        soup = parse(response)
-        # Get headline act & date. Append to lists.
+
+        # Get HTML response & parse
+        r = get_html_response(url)
+        soup = parse_html(response=r)
+
+        # Get headline act & date
         d = event_date(soup)
         e = event_name(soup)
         dates.append(d)
         events.append(e)
 
-    # Convert dates to datetime objects
-    dt = convert_to_dt(dates)
-    # Format dates (dd-mmm-yyyy) and weekday
-    str_dates = string_dates(dt)
-    str_days = string_weekdays(dt)
+    # Intialise dictionary
+    data = {"DT": [], "Date": [], "Event": [], "Venue": [], "URL": []}
+
+    # Parse date into datetime object. Re-format into string.
+    dt = convert_datetime(dates)
+    dt_dates = string_dates(dt)
 
     # Generate list with venue name
-    venue_list = list(itertools.repeat("Phoenix Central Park", len(urls)))
+    venues = list(itertools.repeat("Phoenix Central Park", len(dates)))
 
-    # Create table
-    table = prepare_table(
-        datetime_objects=dt,
-        str_dates=str_dates,
-        weekday=str_days,
-        events=events,
-        venue=venue_list,
-        urls=urls,
-    )
+    # Update Dictionary
+    data["DT"] += dt
+    data["Date"] += dt_dates
+    data["Event"] += events
+    data["Venue"] += venues
+    data["URL"] += urls
 
-    print(*table, sep="\n")
+    # Create DataFrame
+    df = table(data)
+    write_csv(df)
 
 
-def get_response(url):
-    """Get HTML response"""
+def get_html_response(url: str) -> requests.models.Response:
     try:
         headers = {
             "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36"
         }
         r = requests.get(url, headers=headers, timeout=5)
         r.raise_for_status()
-        return r
     except requests.exceptions.HTTPError as http_err:
-        print(f"HTTP error occurred: {http_err}")
+        sys.exit(f"HTTP error occurred: \n'{http_err}'")
+    except requests.exceptions.Timeout as timeout_err:
+        sys.exit(f"Timeout error occurred: \n'{timeout_err}'")
     except Exception as err:
-        print(f"Other error occurred: {err}")
+        sys.exit(f"Error occurred: \n'{err}'")
+    return r
 
 
-def parse(r: requests.models.Response) -> bs4.BeautifulSoup:
-    """Parse HTML"""
-    return BeautifulSoup(r.text, features="lxml")
+def parse_html(response: requests.models.Response) -> bs4.BeautifulSoup:
+    return BeautifulSoup(response.text, features="lxml")
 
 
 def get_urls(soup: bs4.BeautifulSoup) -> list[str]:
-    """Obtain event URLs from webpage"""
     links = [link["href"] for link in soup.findAll("a", href=True)]
     return list({url for url in links if "https://phoenixcentralpark.com.au" in url})
 
 
 def event_date(soup: bs4.BeautifulSoup) -> str:
-    """Obtain date and time of event"""
     extract = soup.findAll("p", class_="sqsrte-large")
     date = extract[-1].text if len(extract) != 1 else extract[0].text
     d = re.split("(2023)", date)
@@ -89,12 +88,10 @@ def event_date(soup: bs4.BeautifulSoup) -> str:
 
 
 def event_name(soup: bs4.BeautifulSoup) -> str:
-    """Extracts name of event"""
     return soup.find("h1").text
 
 
-def convert_to_dt(dates: list[str]) -> list[datetime]:
-    """Parse dates into datetime objects"""
+def convert_datetime(dates: list[str]) -> list[datetime]:
     dt_objects = []
     for d in dates:
         i = datetime.strptime(d, "%d %B %Y")
@@ -103,32 +100,25 @@ def convert_to_dt(dates: list[str]) -> list[datetime]:
 
 
 def string_dates(dt: list[datetime]) -> list[str]:
-    """Format datetime objects into dd-mmm-yyyy format"""
+    """Format datetime objects into dd-mmm-yy format"""
     str_dates = []
     for d in dt:
-        i = d.strftime("%d-%b-%-y")
+        i = d.strftime("%d-%b-%-y (%a)")
         str_dates.append(i)
     return str_dates
 
 
-def string_weekdays(dt: list[datetime]) -> list[str]:
-    """Format datetime objects into weekday"""
-    str_days = []
-    for day in dt:
-        i = day.strftime("%a")
-        str_days.append(i)
-    return str_days
+def table(dict_data: dict) -> pd.DataFrame:
+    return pd.DataFrame(dict_data)
 
 
-def prepare_table(
-    datetime_objects: list[datetime],
-    str_dates: list[str],
-    weekday: list[str],
-    events: list[str],
-    venue: list[str],
-    urls: list[str],
-) -> list:
-    return list(zip(datetime_objects, str_dates, weekday, events, venue, urls))
+# !! DELETE
+def write_csv(df):
+    now = datetime.now()
+    dt_string = now.strftime("%Y-%m-%d_%H%M%S")
+    filepath = f"/Users/darrenchung/Desktop/phoenix-gigs_{dt_string}.csv"
+    print(f"CSV file saved at {filepath}")
+    return df.to_csv(filepath, index=False)
 
 
 if __name__ == "__main__":
