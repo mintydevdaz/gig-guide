83580a2da8713808a9b8810bdb81e8346dfdd2bc
mintydevdaz <mintydevdaz@proton.me>
2023-01-23 12:29:25 +1100

    C#16: created database.py

---
+++ gig_guide/database.py
@@ -0,0 +1,15 @@
+moshtix_links = {
+    "Big Top Luna Park": "https://moshtix.com.au/v2/venues/big-top-luna-park-sydney/12",
+    "Lazybones Lounge": "https://www.moshtix.com.au/v2/venues/lazybones-lounge-restaurant-bar/7848",
+    "Oxford Art Factory": "https://www.moshtix.com.au/v2/venues/oxford-art-factory-sydney/867",
+    "UNSW Roundhouse": "https://moshtix.com.au/v2/venues/roundhouse-sydney/756",
+    "Lansdowne Hotel": "https://www.moshtix.com.au/v2/venues/the-lansdowne-hotel-sydney/4775",
+}
+
+century_links = {
+    "Enmore Theatre": "https://www.enmoretheatre.com.au/?s&key=upcoming",
+    "Metro Theatre": "https://www.metrotheatre.com.au/?s&key=upcoming",
+    "Factory Theatre": "https://www.factorytheatre.com.au/?s&key=upcoming",
+    "The Concourse": "https://www.theconcourse.com.au/?s&key=upcoming",
+    "Manning Bar": "https://www.manningbar.com/?s&key=upcoming",
+}
