18af78eb125191c1baa554e2d9a58057b4ddad1b
mintydevdaz <mintydevdaz@proton.me>
2023-01-23 12:28:54 +1100

    C#14: renamed file

--- gig_guide/moshtix_urls.py
+++
@@ -1,8 +0,0 @@
-links = {
-    "Big Top Luna Park": "https://moshtix.com.au/v2/venues/big-top-luna-park-sydney/12",
-    "Lazybones Lounge": "https://www.moshtix.com.au/v2/venues/lazybones-lounge-restaurant-bar/7848",
-    "Oxford Art Factory": "https://www.moshtix.com.au/v2/venues/oxford-art-factory-sydney/867",
-    "UNSW Roundhouse": "https://moshtix.com.au/v2/venues/roundhouse-sydney/756",
-    "Lansdowne Hotel": "https://www.moshtix.com.au/v2/venues/the-lansdowne-hotel-sydney/4775"
-}
-
