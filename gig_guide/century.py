a7af92e764f6e448a1290b302ac09b0e317ed793
mintydevdaz <mintydevdaz@proton.me>
2023-01-23 22:01:33 +1100

    C#25: updated file

--- gig_guide/century.py
+++ gig_guide/century.py
@@ -1,5 +1,5 @@
+import sys
 from datetime import datetime
-from operator import itemgetter
 
 import bs4
 import pandas as pd
@@ -12,81 +12,63 @@ def main():
     dates = []
     events = []
     venues = []
-    links = []
+    urls = []
 
     for venue, url in century_links.items():
 
-        print(f"Retrieving data for {venue}")
-
-        # Request HTML from venue's webpage
-        request = get_html(url)
-        soup = parse(request)
+        # Get HTML response & parse. Access HTML IDs.
+        r = get_html_response(url)
+        soup = parse_html(response=r)
         ids = get_html_ids(soup)
 
         for i in ids:
-            # Request HTML frim individual event webpage
-            sub_url = get_new_url(url, i)
-            sub_request = get_html(sub_url)
-            soup_cup = parse(sub_request)
 
-            # Get event's date
-            date = event_date(soup_cup)
-            dates.append(date)
+            # Get HTML response & parse. Access unique event page.
+            sub_url = get_link(url, html_id=i)
+            req = get_html_response(sub_url)
+            soup_cup = parse_html(req)
 
-            # Get event name
-            event = event_name(soup_cup)
-            events.append(event)
-
-            # Get URL link
-            links.append(sub_url)
-
-            # Add venue
+            # Get event date, title, venue & url
+            dates.append(event_date(soup_cup))
+            events.append(event_title(soup_cup))
             venues.append(venue)
+            urls.append(sub_url)
 
-    # Convert dates to datetime objects
-    dt = convert_to_dt(dates)
-    # Format dates (dd-mmm-yyyy) and weekday
-    str_dates = string_dates(dt)
-    str_days = string_weekdays(dt)
+    # Parse date into datetime object. Re-format into string.
+    dt = convert_datetime(dates)
+    dt_dates = string_dates(dt)
 
-    # Create table
-    gigs = prepare_table(
-        datetime_objects=dt,
-        str_dates=str_dates,
-        weekday=str_days,
-        events=events,
-        venues=venues,
-        urls=links,
-    )
+    # Intialise & update dictionary
+    data = {"DT": [], "Date": [], "Event": [], "Venue": [], "URL": []}
+    data["DT"] += dt
+    data["Date"] += dt_dates
+    data["Event"] += events
+    data["Venue"] += venues
+    data["URL"] += urls
 
-    # Sort by datetime
-    final_sort = sorted(gigs, key=itemgetter(0))
-
-    # Construct table and remove first column
-    df = table(final_sort)
-    df2 = remove_col(df)
-
-    # Save CSV file
-    write_csv(df2)
+    # Create DataFrame
+    df = table(data)
+    write_csv(df)
 
 
-def get_html(url: str) -> requests.models.Response:
-    # Get request from URL
+def get_html_response(url: str) -> requests.models.Response:
     try:
         headers = {
             "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36"
-        }  # noqa
+        }
         r = requests.get(url, headers=headers, timeout=5)
-    except requests.exceptions.HTTPError as err:
-        print(err)
-    except requests.exceptions.Timeout as err:
-        print(err)
+        r.raise_for_status()
+    except requests.exceptions.HTTPError as http_err:
+        sys.exit(f"HTTP error occurred: \n'{http_err}'")
+    except requests.exceptions.Timeout as timeout_err:
+        sys.exit(f"Timeout error occurred: \n'{timeout_err}'")
+    except Exception as err:
+        sys.exit(f"Error occurred: \n'{err}'")
     return r
 
 
-def parse(r: requests.models.Response) -> bs4.element.ResultSet:
-    """Parse HTML"""
-    return BeautifulSoup(r.text, features="lxml")
+def parse_html(response: requests.models.Response) -> bs4.element.ResultSet:
+    return BeautifulSoup(response.text, features="lxml")
 
 
 def get_html_ids(soup: bs4.element.ResultSet) -> list[str]:
@@ -94,7 +76,7 @@ def get_html_ids(soup: bs4.element.ResultSet) -> list[str]:
     return [tag["id"] for tag in soup.select("a[id]")]
 
 
-def get_new_url(url: str, html_id: str) -> str:
+def get_link(url: str, html_id: str) -> str:
     link = url.removesuffix("?s&key=upcoming")
     return f"{link}event/{html_id}/"
 
@@ -103,63 +85,36 @@ def event_date(soup: bs4.element.ResultSet) -> str:
     return soup.find("li", class_="session-date").text
 
 
-def event_name(soup: bs4.element.ResultSet) -> str:
+def event_title(soup: bs4.element.ResultSet) -> str:
     return soup.find("h1", class_="title").text
 
 
-def convert_to_dt(dates: list[str]) -> list[datetime]:
-    """Parse dates into datetime objects"""
-    dt_objects = []
+def convert_datetime(dates: list[str]) -> list[datetime]:
+    res = []
     for d in dates:
         i = datetime.strptime(d, "%A, %d %B %Y %I:%M %p")
-        dt_objects.append(i)
-    return dt_objects
+        res.append(i)
+    return res
 
 
-def string_dates(dt: list[datetime]) -> list[str]:
-    """Format datetime objects into dd-mmm-yyyy format"""
-    str_dates = []
-    for d in dt:
-        i = d.strftime("%d-%b-%-y")
-        str_dates.append(i)
-    return str_dates
+def string_dates(dt_objects: list[datetime]) -> list[str]:
+    """Format datetime objects into dd-mmm-yy format"""
+    res = []
+    for dt in dt_objects:
+        i = dt.strftime("%d-%b-%-y (%a)")
+        res.append(i)
+    return res
 
 
-def string_weekdays(dt: list[datetime]) -> list[str]:
-    """Format datetime objects into weekday"""
-    str_days = []
-    for day in dt:
-        i = day.strftime("%a")
-        str_days.append(i)
-    return str_days
+def table(dict_data: dict) -> pd.DataFrame:
+    return pd.DataFrame(dict_data)
 
 
-def prepare_table(
-    datetime_objects: list[datetime],
-    str_dates: list[str],
-    weekday: list[str],
-    events: list[str],
-    venues: list[str],
-    urls: list[str],
-) -> list:
-    return list(zip(datetime_objects, str_dates, weekday, events, venues, urls))
-
-
-def table(gigs: list) -> pd.DataFrame:
-    """Table of gigs"""
-    return pd.DataFrame(
-        gigs, columns=["dt", "Date", "Weekday", "Event", "Venue", "Link"]
-    )
-
-
-def remove_col(df):
-    return df.drop("dt", axis="columns")
-
-
+# !! DELETE
 def write_csv(df):
     now = datetime.now()
     dt_string = now.strftime("%Y-%m-%d_%H%M%S")
-    filepath = f"/Users/darrenchung/Desktop/coolgigs_{dt_string}.csv"
+    filepath = f"/Users/darrenchung/Desktop/century-gigs_{dt_string}.csv"
     print(f"CSV file saved at {filepath}")
     return df.to_csv(filepath, index=False)
 
